d8abf62edb7a46153ab28a6792106366339d06cb
JvillapEdu <Jvillap@eafit.edu.co>
2026-09-03 11:06:59 -0500

    Update example text and question in Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -17,10 +17,10 @@ La aplicación aplica normalización y *stemming* para que palabras como *playin
 # Ejemplo inicial en inglés
 text_input = st.text_area(
     "Escribe tus documentos (uno por línea, en inglés):",
-    "The dog barks loudly.\nThe cat meows at night.\nThe dog and the cat play together."
+    "The rat bites.\nThe cheese smells bad.\nThe rat eats the cheese"
 )
 
-question = st.text_input("Escribe una pregunta (en inglés):", "Who is playing?")
+question = st.text_input("Escribe una pregunta (en inglés):", "Who is eating?")
 
 # Inicializar stemmer para inglés
 stemmer = SnowballStemmer("english")
